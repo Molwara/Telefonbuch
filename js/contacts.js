d1cec64c5fef5a2dda70ba8f3a116013f4861c65
Molwara <annikabokelmann@online.de>
2016-12-18 18:51:29 +0100

    Kontakt suchen
    Eingabemaske für Suche hinzugefügt und Filter auf die Datentabelle
    angewandt (+ändere Wert "number" zu "phone")

--- js/contacts.js
+++ js/contacts.js
@@ -1,14 +1,14 @@
 angular.module('contactApp', [])
 	.controller('ContactListController', function() {
 		var contactList = this;
-		contactList.contacts = [{name:'Annika Bokelmann', number:'0176/21360084'},
-						 {name:'Dr. Ralf Itter', number:'+ 49 (0) 551 900 37 90'},
-						 {name:'dff solutions GmbH', number:'+49 (0) 551 / 900 379 – 0'}];
+		contactList.contacts = [{name:'Annika Bokelmann', phone:'0176/21360084'},
+						 {name:'Dr. Ralf Itter', phone:'+ 49 (0) 551 900 37 90'},
+						 {name:'dff solutions GmbH', phone:'+49 (0) 551 / 900 379 – 0'}];
 		
 		contactList.addContact = function() {
-			contactList.contacts.push({name:contactList.name, number:contactList.number});
+			contactList.contacts.push({name:contactList.name, phone:contactList.phone});
 			contactList.name = '';
-			contactList.number = '';
+			contactList.phone = '';
 		};
 		
 		contactList.deleteContact = function(index) {
