b53bc0ceab4fd111334b2683f9ac93ce646d1b1c
Molwara <annikabokelmann@online.de>
2016-12-18 22:01:48 +0100

    Kontakt bearbeiten
    Kontakt Objekt um Atrribut "edit" erweitert, um den Bearbeitungsstatus
    der einzelnen Kontakte abfragen zu können, und Erweiterung der Tabelle
    um einen Bearbeiten Button, der statt dem Text "inputs" einblended, um
    den Kontakt bearbeiten zu können (Hier muss noch an der User-Experience
    gearbeitet werden!)

--- js/contacts.js
+++ js/contacts.js
@@ -1,12 +1,12 @@
 angular.module('contactApp', [])
 	.controller('ContactListController', function() {
 		var contactList = this;
-		contactList.contacts = [{name:'Annika Bokelmann', phone:'0176/21360084'},
-						 {name:'Dr. Ralf Itter', phone:'+ 49 (0) 551 900 37 90'},
-						 {name:'dff solutions GmbH', phone:'+49 (0) 551 / 900 379 – 0'}];
+		contactList.contacts = [{name:'Annika Bokelmann', phone:'0176/21360084', edit: false},
+						 {name:'Dr. Ralf Itter', phone:'+ 49 (0) 551 900 37 90', edit: false},
+						 {name:'dff solutions GmbH', phone:'+49 (0) 551 / 900 379 – 0', edit: false}];
 		
 		contactList.addContact = function() {
-			contactList.contacts.push({name:contactList.name, phone:contactList.phone});
+			contactList.contacts.push({name:contactList.name, phone:contactList.phone, edit: false});
 			contactList.name = '';
 			contactList.phone = '';
 		};
