ec8f3559ea7a5101b15960583168e29bac699f24
Molwara <annikabokelmann@online.de>
2016-12-18 18:01:11 +0100

    Kontakt hinzufügen
    Modul um "addContact" Funktion erweitert und Eingabeformular hinzugefügt

--- js/contacts.js
+++ js/contacts.js
@@ -1,8 +1,13 @@
 angular.module('contactApp', [])
 	.controller('ContactListController', function() {
-		this.contacts = [{name:'Annika Bokelmann', number:'0176/21360084'},
+		var contactList = this;
+		contactList.contacts = [{name:'Annika Bokelmann', number:'0176/21360084'},
 						 {name:'Dr. Ralf Itter', number:'+ 49 (0) 551 900 37 90'},
 						 {name:'dff solutions GmbH', number:'+49 (0) 551 / 900 379 – 0'}];
 		
-		
+		contactList.addContact = function() {
+			contactList.contacts.push({name:contactList.name, number:contactList.number});
+			contactList.name = '';
+			contactList.number = '';
+		}
 	});
