8831b072966c65023fb69dba66ad6df589cff6bd
Molwara <annikabokelmann@online.de>
2016-12-20 17:36:27 +0100

    Kontakt bearbeiten Button User-Experience verbessert

--- js/contacts.js
+++ js/contacts.js
@@ -4,6 +4,8 @@ angular.module('contactApp', [])
 		contactList.contacts = [{name:'Annika Bokelmann', phone:'0176/21360084', edit: false},
 						 {name:'Dr. Ralf Itter', phone:'+ 49 (0) 551 900 37 90', edit: false},
 						 {name:'dff solutions GmbH', phone:'+49 (0) 551 / 900 379 – 0', edit: false}];
+		contactList.textEdit = 'Bearbeiten';
+		contactList.textSave = 'Speichern';
 		
 		contactList.addContact = function() {
 			contactList.contacts.push({name:contactList.name, phone:contactList.phone, edit: false});
@@ -13,5 +15,9 @@ angular.module('contactApp', [])
 		
 		contactList.deleteContact = function(index) {
 			contactList.contacts.splice(index,1);
+		};
+		
+		contactList.toggleEdit = function(index) {
+			contactList.contacts[index].edit === false ? contactList.contacts[index].edit = true : contactList.contacts[index].edit = false
 		};
 	});
